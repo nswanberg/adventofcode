01e3551d568ffca595438795c96f7645289cf45f
Nate Swanberg <nate@m1mb.local>
2020-12-27 12:41:04 -0700

    final problem (21)

--- 2020/21.py
+++ 2020/21.py
@@ -1,39 +1,67 @@
 from collections import defaultdict
 
-with open('21-small.txt') as f:
+with open('21.txt') as f:
   input = f.read()
 
 foods = input.strip().splitlines()
 
-allingredients=set()
-candidates=defaultdict(list)
+allingredients=defaultdict(int)
 allallergens=defaultdict(set)
-pairs=defaultdict(int)
-options=defaultdict(int)
 for food in foods:
   ingredientstext, allergenstext = food.replace(')','').replace(',','').split(' (contains ')
   ingredients = ingredientstext.split(' ')
   allergens = allergenstext.split(' ')
+  #print(allergens)
+  #print(ingredients)
+  # append allergens listed in this food to each ingredient
   for i in ingredients:
-    allingredients.add(i)
-    candidates[i].append(tuple(allergens))
-    options[(tuple(allergens),i)]+=1
-    for a in allergens:
-      allallergens[a].add(i)
-      pairs[(a,i)]+=1
-  if len(a) == 1:
-    removelist=[]
-    for i in allergens[a]:
-      if i not in ingredients:
-        removelist.append(i)
-    for r in removelist:
-      allergens[a].remove(r)
+    allingredients[i]+=1
+  for a in allergens:
+    if len(allallergens[a]) == 0:
+      allallergens[a].update(ingredients)
+    else:
+      removals=[]
+      for i in allallergens[a]:
+        if i not in ingredients:
+          removals.append(i)
+      for r in removals:
+        allallergens[a].remove(r)
 
-#print(allingredients)
-#for c in candidates:
-#  print(c, candidates[c])
-#for p in pairs:
-#  print(p, pairs[p])
-#print('allallergens',allallergens)
-for o in options:
-  print(o, options[o])
+print('allergens')
+for a in allallergens.keys():
+  print(a, allallergens[a])
+known=set()
+for v in allallergens.values():
+  known.update(v)
+for k in known:
+  allingredients.pop(k)
+print(sum(allingredients.values()))
+
+# part 2
+
+knowningredients={}
+while max([len(a) for a in allallergens.values()]) > 1:
+  print(knowningredients)  
+  print(allallergens)
+  for a in allallergens.keys():
+    if a in knowningredients.keys():
+      continue
+    if len(allallergens[a]) == 1:
+      knowningredients[a]=list(allallergens[a])[0]
+      continue
+    removals=[]
+    for i in allallergens[a]:
+      if i in knowningredients.values():
+        removals.append(i)
+    for r in removals:
+      allallergens[a].remove(r)
+    if len(allallergens[a]) == 1:
+      knowningredients[a]=list(allallergens[a])[0]
+      continue
+
+knownkeys=list(knowningredients.keys())
+knownkeys.sort()
+known=[]
+for k in knownkeys:
+  known.append(knowningredients[k])
+print(','.join(known))  
